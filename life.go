dfa6cbea3edf377a120001ce01667caadc911033
ZhilenkovAndrey <dron19812013@gmail.com>
2024-12-05 13:39:07 +0300

    2

--- life.go
+++ life.go
@@ -8,14 +8,14 @@ import (
 type Universe [][]bool
 
 const (
-	width  = 80
+	width  = 20
 	height = 15
 )
 
 func NewUniverse() Universe {
-	matrix := make(Universe, height)
+	matrix := make(Universe, width)
 	for i := range matrix {
-		matrix[i] = make([]bool, width)
+		matrix[i] = make([]bool, height)
 	}
 	return matrix
 }
@@ -32,7 +32,6 @@ func (matrix Universe) Seed() Universe {
 	for i := range matrix {
 		for j := range matrix[i] {
 			b := rand.Intn(4)
-			fmt.Println(b)
 			if b == 1 && col != 0 {
 				matrix[i][j] = true
 				col--
@@ -42,6 +41,41 @@ func (matrix Universe) Seed() Universe {
 	return matrix
 }
 
+func (matrix Universe) IsCellAlive(x, y int) bool {
+	if x < 0 {
+		x = (x % width) + width
+	}
+
+	if y < 0 {
+		y = (y % height) + height
+	}
+
+	if x >= width {
+		x = x % width
+	}
+
+	if y >= height {
+		y = y % height
+	}
+
+	return matrix[x][y]
+}
+
+func (matrix Universe) SelsNeighbors(x, y int) int {
+	sum := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if !(i == 0 && j == 0) && matrix.IsCellAlive(x+j, y+i) {
+				sum++
+			}
+		}
+	}
+	return sum
+}
+
 func main() {
-	NewUniverse().Seed().Show()
+	m := NewUniverse()
+	m.Seed().Show()
+	fmt.Println(m.SelsNeighbors(1, 1))
+	fmt.Println(m.IsCellAlive(1, 1))
 }
